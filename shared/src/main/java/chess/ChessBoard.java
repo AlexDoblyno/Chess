e3cbdc76f32d68ccbce7ba8c88609669323176ba
AlexDoblyno <alexdoblyno@gmail.com>
2025-02-07 16:39:09 -0700

    Add files via upload

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,30 +1,43 @@
-
 package chess;
 
-public class ChessGame {
-    public ChessGame() { /* compiled code */ }
+/**
+ * A chessboard that can hold and rearrange chess pieces.
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessBoard {
 
-    public chess.ChessGame.TeamColor getTeamTurn() { /* compiled code */ }
+    public ChessBoard() {
+        
+    }
 
-    public void setTeamTurn(chess.ChessGame.TeamColor team) { /* compiled code */ }
+    /**
+     * Adds a chess piece to the chessboard
+     *
+     * @param position where to add the piece to
+     * @param piece    the piece to add
+     */
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+        throw new RuntimeException("Not implemented");
+    }
 
-    public java.util.Collection<chess.ChessMove> validMoves(chess.ChessPosition startPosition) { /* compiled code */ }
+    /**
+     * Gets a chess piece on the chessboard
+     *
+     * @param position The position to get the piece from
+     * @return Either the piece at the position, or null if no piece is at that
+     * position
+     */
+    public ChessPiece getPiece(ChessPosition position) {
+        throw new RuntimeException("Not implemented");
+    }
 
-    public void makeMove(chess.ChessMove move) throws chess.InvalidMoveException { /* compiled code */ }
-
-    public boolean isInCheck(chess.ChessGame.TeamColor teamColor) { /* compiled code */ }
-
-    public boolean isInCheckmate(chess.ChessGame.TeamColor teamColor) { /* compiled code */ }
-
-    public boolean isInStalemate(chess.ChessGame.TeamColor teamColor) { /* compiled code */ }
-
-    public void setBoard(chess.ChessBoard board) { /* compiled code */ }
-
-    public chess.ChessBoard getBoard() { /* compiled code */ }
-
-    public static enum TeamColor {
-        WHITE, BLACK;
-
-        private TeamColor() { /* compiled code */ }
+    /**
+     * Sets the board to the default starting board
+     * (How the game of chess normally starts)
+     */
+    public void resetBoard() {
+        throw new RuntimeException("Not implemented");
     }
 }
