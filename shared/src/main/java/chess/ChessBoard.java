242205681796502febe65926aef6e72e8b49b3b8
AlexDoblyno <alexdoblyno@gmail.com>
2025-02-07 16:27:50 -0700

    Update ChessBoard.java

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,1 +1,30 @@
 
+package chess;
+
+public class ChessGame {
+    public ChessGame() { /* compiled code */ }
+
+    public chess.ChessGame.TeamColor getTeamTurn() { /* compiled code */ }
+
+    public void setTeamTurn(chess.ChessGame.TeamColor team) { /* compiled code */ }
+
+    public java.util.Collection<chess.ChessMove> validMoves(chess.ChessPosition startPosition) { /* compiled code */ }
+
+    public void makeMove(chess.ChessMove move) throws chess.InvalidMoveException { /* compiled code */ }
+
+    public boolean isInCheck(chess.ChessGame.TeamColor teamColor) { /* compiled code */ }
+
+    public boolean isInCheckmate(chess.ChessGame.TeamColor teamColor) { /* compiled code */ }
+
+    public boolean isInStalemate(chess.ChessGame.TeamColor teamColor) { /* compiled code */ }
+
+    public void setBoard(chess.ChessBoard board) { /* compiled code */ }
+
+    public chess.ChessBoard getBoard() { /* compiled code */ }
+
+    public static enum TeamColor {
+        WHITE, BLACK;
+
+        private TeamColor() { /* compiled code */ }
+    }
+}
