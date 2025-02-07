5d4c2b946166ae105e1a5ae97ebe805c64fdd22d
AlexDoblyno <alexdoblyno@gmail.com>
2025-02-07 16:36:27 -0700

    Add files via upload

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -162,7 +162,163 @@ public class ChessGame {
         turnTeam = (turnTeam == TeamColor.WHITE) ? TeamColor.BLACK : TeamColor.WHITE;
     }
 
+    /**
+     * Determines if the given team is in check
+     *
+     * @param teamColor which team to check for check
+     * @return True if the specified team is in check
+     */
+    public boolean isInCheck(TeamColor teamColor) {
+        Collection<ChessPosition> enemyPositions = gameBoard.getPieces(
+                (teamColor == TeamColor.BLACK) ? TeamColor.WHITE : TeamColor.BLACK);
+        for (ChessPosition position : enemyPositions) {
+            if (gameBoard.getPiece(position).canKillKing(gameBoard, position)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Check if the piece of teamColor in position piecePosition is in danger (used for castling)
+    public boolean isInDanger(TeamColor teamColor, ChessPosition piecePosition) {
+        Collection<ChessPosition> enemyPositions = gameBoard.getPieces(
+                (teamColor == TeamColor.BLACK) ? TeamColor.WHITE : TeamColor.BLACK);
+        for (ChessPosition position : enemyPositions) {
+            if (gameBoard.getPiece(position).canKillPiece(gameBoard, position, piecePosition)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Determines if the given team is in checkmate
+     *
+     * @param teamColor which team to check for checkmate
+     * @return True if the specified team is in checkmate
+     */
+    public boolean isInCheckmate(TeamColor teamColor) {
+        if (!isInCheck(teamColor)) {
+            return false;
+        }
+        Collection<ChessPosition> team = gameBoard.getPieces(teamColor);
+        for (ChessPosition position : team) {
+            Collection<ChessMove> moves = validMoves(position);
+            for (ChessMove move : moves) {
+                ChessPiece capturedPiece = gameBoard.doMove(move);
+                if (!isInCheck(teamColor)) {
+                    gameBoard.undoMove(move, capturedPiece);
+                    return false;
+                } else {
+                    gameBoard.undoMove(move, capturedPiece);
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Determines if the given team is in stalemate, which here is defined as having
+     * no valid moves
+     *
+     * @param teamColor which team to check for stalemate
+     * @return True if the specified team is in stalemate, otherwise false
+     */
+    public boolean isInStalemate(TeamColor teamColor) {
+        if (isInCheck(teamColor)) {
+            return false;
+        }
+        Collection<ChessPosition> team = gameBoard.getPieces(teamColor);
+        for (ChessPosition position : team) {
+            Collection<ChessMove> moves = validMoves(position);
+            if (!moves.isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Gets the current chessboard
+     *
+     * @return the chessboard
+     */
+    public ChessBoard getBoard() {
+        return gameBoard;
+    }
+
+    /**
+     * Sets this game's chessboard with a given board
+     *
+     * @param board the new board to use
+     */
+    public void setBoard(ChessBoard board) {
+        gameBoard = board;
+        blackKingMoved = false;
+        whiteKingMoved = false;
+        blackLeftRookMoved = false;
+        whiteLeftRookMoved = false;
+        blackRightRookMoved = false;
+        whiteRightRookMoved = false;
         pawnMovedTwo = null;
     }
-}
 
+    // Used to set special moves variables (mainly to keep track of if kings and rooks have moved, as well as keep
+    // track of the pawn that just moved two)
+    private void checkMove(ChessMove move) {
+        ChessPiece piece = gameBoard.getPiece(move.getStartPosition());
+        switch (piece.getPieceType()) {
+            case PAWN:
+                if (abs(move.getStartPosition().getRow() - move.getEndPosition().getRow()) == 2) {
+                    pawnMovedTwo = move.getEndPosition();
+                }
+                break;
+            case ROOK:
+                if (piece.getTeamColor() == TeamColor.BLACK && move.getStartPosition().getRow() == 8) {
+                    if (move.getStartPosition().getColumn() == 1) {
+                        blackLeftRookMoved = true;
+                    } else if (move.getStartPosition().getColumn() == 8) {
+                        blackRightRookMoved = true;
+                    }
+                } else if (piece.getTeamColor() == TeamColor.WHITE && move.getStartPosition().getRow() == 1) {
+                    if (move.getStartPosition().getColumn() == 1) {
+                        whiteLeftRookMoved = true;
+                    } else if (move.getStartPosition().getColumn() == 8) {
+                        whiteRightRookMoved = true;
+                    }
+                }
+                break;
+            case KING:
+                if (piece.getTeamColor() == TeamColor.BLACK && move.getStartPosition()
+                        .getRow() == 8 && move.getStartPosition().getColumn() == 5) {
+                    blackKingMoved = true;
+                } else if (piece.getTeamColor() == TeamColor.WHITE && move.getStartPosition()
+                        .getRow() == 1 && move.getStartPosition().getColumn() == 5) {
+                    whiteKingMoved = true;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public Boolean isOver() {
+        if (isInStalemate(TeamColor.BLACK) || isInStalemate(TeamColor.WHITE)
+            || (isInCheckmate(TeamColor.BLACK) && turnTeam.equals(TeamColor.BLACK))
+            || (isInCheckmate(TeamColor.WHITE) && turnTeam.equals(TeamColor.WHITE))) {
+            gameOver = true;
+        }
+        return gameOver;
+    }
+
+    public void setGameOver(boolean gameOver) {
+        this.gameOver = gameOver;
+    }
+
+    /**
+     * Enum identifying the 2 possible teams in a chess game
+     */
+    public enum TeamColor {
+        WHITE, BLACK
+    }
+}
